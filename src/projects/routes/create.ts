d7e9841e40c34ffb0775dd44f6ee1332c5b16be4
Aaron Adrian <aaroncadrian@gmail.com>
2020-09-19 14:27:53 -0500

    Add initial route

---
+++ src/projects/routes/create.ts
@@ -0,0 +1,6 @@
+module.exports.createProject_2020_05_01 = (req, res, next) => {
+    res.json({
+        ok: true,
+        version: '2020-05-01'
+    })
+};
---
+++ src/server.ts
@@ -0,0 +1,10 @@
+const express = require('express');
+const { createProject_2020_05_01 } = require('./projects/routes/create');
+
+const app = express();
+
+app.post('/projects', createProject_2020_05_01);
+
+app.listen(4800, () => {
+    console.log('Server is listening at port 4800');
+});
