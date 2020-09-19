29643f11affeba2406f3124514c2b1d8fa04c139
Aaron Adrian <aaroncadrian@gmail.com>
2020-09-19 15:23:58 -0500

    Use version map

--- src/server.ts
+++ src/server.ts
@@ -10,14 +10,25 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.post('/projects', (req, res) => {
+app.post('/projects', (req, res, next) => {
   const version = req.header('X-Version');
 
-  if (version === '2020-09-19') {
-    createProject_2020_09_19(req, res);
-  } else {
-    createProject_2020_05_01(req, res);
+  const versionMap = {
+    '2020-09-19': createProject_2020_09_19,
+    '2020-05-01': createProject_2020_05_01,
+  };
+
+  const handler = versionMap[version];
+
+  if (!handler) {
+    res.status(403).json({
+      error: `Unsupported Version: ${version}`,
+    });
+
+    return;
   }
+
+  handler(req, res, next);
 });
 
 app.listen(4800, () => {
